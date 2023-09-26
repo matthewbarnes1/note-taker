48a150c7723741fa263ec8c942587c6e4049e382
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-26 17:59:34 -0400

    fixed notes.html routing in index file

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,65 @@
-//* holds get and post for db json data storage and retrieval
-const note = require('express').Router();
+const express = require('express');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');  // For generating unique IDs
+
+const router = express.Router();
+
+// Path to the database
+const dbPath = './db/db.json';
+
+// Helper function to load notes
+const loadNotes = () => {
+    try {
+        const notes = fs.readFileSync(dbPath, 'utf-8');
+        return JSON.parse(notes);
+    } catch (err) {
+        console.error("Error reading the database:", err);
+        return [];
+    }
+};
+
+// Helper function to save notes
+const saveNotes = (notes) => {
+    try {
+        fs.writeFileSync(dbPath, JSON.stringify(notes, null, 4));
+    } catch (err) {
+        console.error("Error writing to the database:", err);
+    }
+};
+
+// GET Route to retrieve all notes
+router.get('/notes', (req, res) => {
+    const notes = loadNotes();
+    res.json(notes);
+});
+
+// POST Route to add a new note
+router.post('/notes', (req, res) => {
+    const notes = loadNotes();
+
+    const newNote = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text
+    };
+
+    notes.push(newNote);
+    saveNotes(notes);
+
+    res.json(newNote);
+});
+
+// DELETE Route to delete a note by its ID
+router.delete('/notes/:id', (req, res) => {
+    const notes = loadNotes();
+    const newNotesList = notes.filter(note => note.id !== req.params.id);
+
+    if (newNotesList.length !== notes.length) {
+        saveNotes(newNotesList);
+        res.json({ message: 'Note deleted successfully' });
+    } else {
+        res.status(404).json({ message: 'Note not found' });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+app.use('/api', api);  // Using your API middleware for all /api routes
 app.use(express.static('public'));
 
 // GET Route for homepage
