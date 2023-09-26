eba1f98fee4e5df487409f35172d1302fb6db816
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-26 18:21:05 -0400

    something is working?

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index.js');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);  
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
