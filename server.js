1c15797e28c2d82f5e44cf098a8b70bb12a1dd14
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-26 18:15:04 -0400

    struggle

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const api = require('./public/assets/js/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,15 +8,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);  // Using your API middleware for all /api routes
+app.use('/api', api);  
 app.use(express.static('public'));
 
-// GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
